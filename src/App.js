05422f487acb5505521c29e5b1d1ef0aac1d6c54
calmworld <sarah.alhuriz@hotmail.com>
2024-07-25 13:19:56 -0600

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import ColorGradiant from './ColorGradiant/ColorGradiant';
 import './App.css';
-import { VisuallyHidden } from './VisuallyHidden';
+// import { VisuallyHidden } from './VisuallyHidden';
 
-function Friend({ name, isOnline }) {
-  return (
-    <li className="friend">
-      {isOnline && <div className="green-dot" />}
-      {name}
-      {!isOnline && <VisuallyHidden>(Offline)</VisuallyHidden>}
-      {isOnline && <VisuallyHidden>(Online)</VisuallyHidden>}
-    </li>
-  );
-}
+// function Friend({ name, isOnline }) {
+//   return (
+//     <li className="friend">
+//       {isOnline && <div className="green-dot" />}
+//       {name}
+//       {!isOnline && <VisuallyHidden>(Offline)</VisuallyHidden>}
+//       {isOnline && <VisuallyHidden>(Online)</VisuallyHidden>}
+//     </li>
+//   );
+// }
 
 function App() {
   return (
     <div className="app">
       <ColorGradiant />
-      <div>
+      {/* <div>
         <ul className="friend-list">
           <Friend name="Andrew" isOnline={false} />
           <Friend name="Beatrice" isOnline={true} />
           <Friend name="Chen" isOnline={true} />
         </ul>
-      </div>
+      </div> */}
     </div>
   )
 }
